727592abdbd213e2083336f3b7a775865253c8d1
Guerinik Abderrahmane <guerinikabderrahmanee@gmail.com>
2026-03-07 11:21:33 +0100

    implement a simple version of self-attention mechanism

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tiktoken
+import torch
+import torch.nn as nn
 from datasets.create_dataloader import create_dataloader_v1
 
 enc = tiktoken.encoding_for_model("gpt-2")
@@ -10,7 +12,7 @@ dataloader = create_dataloader_v1(
     raw_text,
     batch_size=8,
     max_length=4,
-    stride=1,
+    stride=4,
     shuffle=False
 )
 
@@ -20,3 +22,17 @@ inputs, outputs  = next(data_iter)
 print(inputs)
 print('\n')
 print(outputs)
+
+vocab_size = 50257
+output_dim = 256
+context_length = 4
+
+embedding_layer = nn.Embedding(vocab_size, output_dim)
+pos_embedding_layer = nn.Embedding(4, output_dim)
+
+token_embeddings = embedding_layer(inputs)
+
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+input_embeddings = token_embeddings + pos_embeddings
+
+
