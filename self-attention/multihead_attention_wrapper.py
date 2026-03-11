32b0cfe1a1969b593cee97250a1d9a0f7237a8e0
Guerinik Abderrahmane <guerinikabderrahmanee@gmail.com>
2026-03-11 11:36:27 +0100

    implement multi-head attention wrapper

---
+++ self-attention/multihead_attention_wrapper.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+from causal_attention import CausalAttention
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, dropout, context_length, num_heads, qkv_bias=False): 
+        super().__init__()
+        self.heads = nn.ModuleList([
+            CausalAttention(d_in, d_out, dropout, context_length, qkv_bias) for _ in range(num_heads)
+        ])
+        
+    def forward(self, x):
+        return torch.cat((head(x) for head in self.heads), dim=1)
